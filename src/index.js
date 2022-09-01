8a3bbdb1a3e99ebeb865b89abd42bed7c269dc98
Isha Jaiswal <jaiswalisha172@gmail.com>
2022-09-01 17:22:57 +0530

    assignment solved

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ishajais:oAUxEOHtcpqbC8KS@cluster0.uoth0nn.mongodb.net/isha-db" ,{
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
---
+++ src/middleware/auth.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+const authenticate = function(req, res, next) {
+    try{
+    let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+  let decodedToken = jwt.verify(token, "functionup-plutonium");
+if (!decodedToken)
+  return res.send({ status: false, msg: "token is invalid" });
+    }catch(err){
+        res.send(err)
+    }
+    next()
+}
+
+
+const authorise = function(req, res, next) {
+    try{
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "functionup-plutonium");
+    let tokenId = decodedToken.userId
+    let userId = req.params.userId;
+    if(tokenId==userId){
+        next()
+    }
+    else res.send("you are not authorized")
+    // comapre the logged in user's id and the id in request
+}catch(e){res.send(e)}
+} 
+
+const usercheck =async (req,res,next)=>{
+    let userId = req.params.userId;
+    let userDetails = await userModel.findById(userId);
+  if (!userDetails)
+    return res.send({ status: false, msg: "No such user exists" });
+next()
+}
+
+module.exports.authenticate=authenticate
+module.exports.authorise= authorise
+module.exports.usercheck=usercheck
