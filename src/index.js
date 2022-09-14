076f8fd4ab02275ab0a1b963dfde635046ccd65b
kamrebaba <alamkamre58@gmail.com>
2022-09-14 11:30:11 +0530

    initial phase

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express= require ('express')
+const bodyparser= require('body-parser')
+const mongoose= require('mongoose')
+const router= require("../route/routes")
+
+const app= express()
+
+app.use(bodyparser.json())
+mongoose.connect("mongodb+srv://kamrebaba:%3Cironman%3E@cluster0.lvfp80k.mongodb.net/project2nd",{
+    useNewUrlParser:true
+})
+.then(()=> console.log("MongoDb is Connected"))
+.catch(err=> console.log(err))
+
+app.use('/',router)
+
+app.listen(process.env.PORT || 3000, function(req,res){
+    res.send(console.log("express at running on port" +(process.env.PORT || 3000)))
+})
