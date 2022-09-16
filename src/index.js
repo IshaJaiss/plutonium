ea2eb7951d3c56bdd740010991faa26071f4cac5
Isha Jaiswal <jaiswalisha172@gmail.com>
2022-09-17 00:15:06 +0530

    solved project2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express= require ('express')
-const bodyparser= require('body-parser')
-const mongoose= require('mongoose')
-const router= require("./route/routes")
+const express = require('express')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+const router = require("./route/routes")
 
-const app= express()
+const app = express()
 
 app.use(bodyparser.json())
-mongoose.connect("mongodb+srv://kamrebaba:ironman@cluster0.lvfp80k.mongodb.net/group38Database",{
-    useNewUrlParser:true
+mongoose.connect("mongodb+srv://kamrebaba:ironman@cluster0.lvfp80k.mongodb.net/group38Database", {
+    useNewUrlParser: true
 })
-.then(()=> console.log("MongoDb is Connected"))
-.catch(err=> console.log(err))
+    .then(() => console.log("MongoDb is Connected"))
+    .catch(err => console.log(err))
 
-app.use('/',router)
+app.use('/', router)
 
 app.listen(3000, function () {
     console.log("code is running sucsessfully on port 3000")
